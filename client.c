dabe5f7f0041545e38f9c82eb1c0765fd7047983
Constantin Rusu <crusu@c2r3p9.42firenze.it>
2023-04-05 19:08:22 +0200

    Apr5 19:08

--- client.c
+++ client.c
@@ -6,11 +6,27 @@
 /*   By: crusu <crusu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 17:51:17 by crusu             #+#    #+#             */
-/*   Updated: 2023/04/04 01:25:09 by crusu            ###   ########.fr       */
+/*   Updated: 2023/04/05 19:04:43 by crusu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	ft_sender(char let, int pid)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if (let >> bit & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(1);
+		bit++;
+	}
+}
 
 void	ft_pidsender(char *str, int pid)
 {
@@ -30,24 +46,37 @@ void	ft_pidsender(char *str, int pid)
 	}
 }
 
-void	ft_sender(char let, int pid)
+void	ft_getping(int sig)
 {
-	int	bit;
+	static int	status;
 
-	bit = 0;
-	while (bit < 8)
+	if (sig == SIGUSR1)
 	{
-		if (let >> bit & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(1);
-		bit++;
+		write(1, "Sending message\n", 17);
+		status = sig;
+	}
+	else if (sig == SIGUSR2)
+	{
+		status = sig;
+		write(1, "Message received by server\n", 28);
+	}
+	else
+	{
+		if (!status)
+			ft_printf("ERROR: Something went wrong\n");
 	}
 }
 
-int	ft_errno(char *str)
+int	ft_errno(int argc, char **argv)
 {
+	char	*str;
+
+	if (argc != 3)
+	{	
+		ft_printf("ERROR: Ivalid number of arguments\n");
+		return (1);
+	}
+	str = argv[1];
 	while (*str)
 	{
 		if (ft_isdigit(*str))
@@ -66,26 +95,19 @@ int	main(int argc, char *argv[])
 	char	*str;
 	int		pid;
 
-	if (argc != 3)
-	{	
-		ft_printf("ERROR: Ivalid number of arguments\n");
+	signal(SIGUSR1, ft_getping);
+	signal(SIGUSR2, ft_getping);
+	if (ft_errno(argc, argv))
 		return (0);
-	}
-	str = argv[1];
-	if (ft_errno(str))
-		return (0);
-	pid = ft_atoi(str);
-	ft_pidsender(str, pid);
+	pid = ft_atoi(argv[1]);
+	ft_pidsender(ft_itoa(getpid()), pid);
 	str = argv[2];
-	if (sleep(10))
-	{
-		ft_printf("ERROR: Unable to reach server\nTried to send %s to %d", str, pid);
-		return (0);
-	}
 	while (*str)
 	{
 		ft_sender(*str, pid);
 		str++;
 	}
+	ft_sender(0, pid);
+	ft_getping(0);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: crusu <crusu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 15:54:56 by crusu             #+#    #+#             */
-/*   Updated: 2023/04/04 00:57:57 by crusu            ###   ########.fr       */
+/*   Updated: 2023/04/05 19:04:36 by crusu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,19 @@ int	ft_transpid(char n)
 {
 	static char	pid[8];
 	static int	pos;
+	int			clpid;
 
 	if (pos < 8)
 		pid[pos] = n;
 	pos++;
 	if (pos == 8)
-		return (ft_atoi(pid));
-	else
-		return (0);
+	{
+		clpid = ft_atoi(pid);
+		kill(clpid, SIGUSR1);
+		pos = 0;
+		return (clpid);
+	}
+	return (0);
 }
 
 char	add_bit(int bit)
@@ -45,10 +50,15 @@ void	ft_decoder(int sig)
 	bit++;
 	if (sig == SIGUSR1)
 		let += add_bit(bit);
-	if (bit == 8 && clpid)
+	if (bit == 8)
 	{
 		if (!clpid)
 			clpid = ft_transpid(let);
+		else if (let == 0)
+		{
+			kill(clpid, SIGUSR2);
+			clpid = 0;
+		}
 		else
 			write(1, &let, 1);
 		let = 0;
@@ -62,6 +72,5 @@ int	main(void)
 	signal(SIGUSR1, ft_decoder);
 	signal(SIGUSR2, ft_decoder);
 	while (1)
-	{
-	}
+		;
 }
